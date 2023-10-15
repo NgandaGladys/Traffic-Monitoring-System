de26ae83fbcf92eab83253880e727388f8fa13f2
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2023-10-15 19:20:12 +0300

    my code

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 include 'root/config.php'; 
 include 'root/process.php'; 
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,18 +11,17 @@ include 'root/process.php';
     <title>Login Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="src/style.css">
 </head>
 <body>
     <div class="overlay">
         <div class="container">
-            <div class="row m-5 no-gutters shadow-lg" >
-                <div class="col-md-6 d-none d-md-block" >
-                    <img src="images/maxim-abramov-GFjyimhomaM-unsplash.jpg" class="img-fluid" style="min-height:100%; border-radius: 10px 0 0 10px; width: 100%;height: 50px;"/>
-                </div>
-                <div class="col-md-6 bg-white p-5" style="border-radius: 0 10px 10px 0;">
+            <div class="row m-5 no-gutters" >
+                <div class="col-md-12 bg-white p-5" style="border-radius: 0 10px 10px 0;">
                     <div class="form-style">
-                        <center><div class="spinner-border text-dark"></div></center>                    
+                        <center>
+                            <div class="spinner-border text-dark"></div>
+                            <p>Re-directing to Dashboard....</p>     
+                        </center>               
                     </div>
                 </div>
             </div>
