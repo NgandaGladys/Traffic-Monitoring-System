3d302759d65b6b18a39d94b1eb6b83c3abb8712e
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2023-10-15 19:15:52 +0300

    my code

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+include 'root/config.php'; 
+include 'root/process.php'; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/style.css">
+</head>
+<body>
+    <div class="overlay">
+        <div class="container">
+            <div class="row m-5 no-gutters shadow-lg" >
+                <div class="col-md-6 d-none d-md-block" >
+                    <img src="images/maxim-abramov-GFjyimhomaM-unsplash.jpg" class="img-fluid" style="min-height:100%; border-radius: 10px 0 0 10px; width: 100%;height: 50px;"/>
+                </div>
+                <div class="col-md-6 bg-white p-5" style="border-radius: 0 10px 10px 0;">
+                    <div class="form-style">
+                                               
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://use.fontawesome.com/f59bcd8580.js"></script>
+</body>
+</html>
