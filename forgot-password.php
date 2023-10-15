11b3f2d5021003208817179ebb219d5462aa2edd
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2023-10-15 18:18:10 +0300

    my code

--- forgot-password.php
+++ forgot-password.php
@@ -1,6 +1,10 @@
+<?php 
+include 'root/config.php'; 
+include 'root/process.php'; 
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
