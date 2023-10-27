f9d6a55886a3d576cd0f722fca8224fcff7adabc
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2023-10-27 22:36:42 +0300

    code

--- dashboard/edit-route.php
+++ dashboard/edit-route.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <form action="" method="post" enctype="multipart/form-data">
-                <input type="hidden" value="<?=$rx->road_id; ?>" name="road_id">
+                <input type="hidden" value="<?=$rx->rid; ?>" name="rid">
                 <div class="modal-header">
                     <h5 class="modal-title">Edit Route</h5>
                     <button type="button" class="close" data-dismiss="modal">
@@ -11,17 +11,35 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <!-- `road_id`, `road_name` -->
+                    <!-- `rid`, `road_id`, `fromm`, `too`, `status` -->
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="form-group">
                                 <label>Road Name (required)</label>
-                                <input class="form-control" name="road_name" value="<?=$rx->road_name; ?>" type="text" required />
+                                <input class="form-control" readonly name="road_name" value="<?=$rx->road_name; ?>" type="text" required />
+                            </div>
+                        </div>
+                         <div class="col-lg-12">
+                            <div class="form-group">
+                                <label>Road From (required)</label>
+                                <input class="form-control" name="fromm" value="<?=$rx->fromm; ?>" type="text" required />
+                            </div>
+                        </div>
+                         <div class="col-lg-12">
+                            <div class="form-group">
+                                <label>Road too (required)</label>
+                                <input class="form-control" name="too" value="<?=$rx->too; ?>" type="text" required />
+                            </div>
+                        </div>
+                         <div class="col-lg-12">
+                            <div class="form-group">
+                                <label>Road status (required)</label>
+                                <input class="form-control" name="status" value="<?=$rx->status; ?>" type="text" required />
                             </div>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="submit" name="update_road_new_user_btn" class="btn btn-success">Save</button>
+                        <button type="submit" name="update_road_route_new_user_btn" class="btn btn-success">Save</button>
                     </div>
                 </div>
             </form>
