ead5d6e21745dc64178968a171cd9260fb8ad8d5
tracymuzaki <tracymuzaki12@gmail.com>
2023-12-15 01:28:25 +0300

    updates

--- dashboard/header.php
+++ dashboard/header.php
@@ -57,87 +57,107 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
    </div>
    <div class="wrapper">
       <!-- Navbar-->
-      <header class="main-header-top hidden-print">
-         <a href="<?=HOME_URL; ?>" class="logo">
-            <!-- <img class="img-fluid able-logo" src="assets/images/tms-logo.png" alt="Theme-logo"> -->
-            Traffic Monitoring System
-        </a>
-         <nav class="navbar navbar-static-top">
-            <!-- Sidebar toggle button-->
-            <a href="#!" data-toggle="offcanvas" class="sidebar-toggle"></a>
-            <!-- Navbar Right Menu-->
-            <div class="navbar-custom-menu f-right">
-               <ul class="top-nav">          
-                  <!-- window screen -->
-                  <!-- <li class="pc-rheader-submenu">
-                     <a href="#!" class="drop icon-circle" onclick="javascript:toggleFullScreen()">
-                        <i class="icon-size-fullscreen"></i>
-                     </a>
+        <header class="main-header-top hidden-print">
+            <a href="<?=HOME_URL; ?>" class="logo">
+                <!-- <img class="img-fluid able-logo" src="assets/images/tms-logo.png" alt="Theme-logo"> -->
+                Traffic Monitoring System
+            </a>
+            <nav class="navbar navbar-static-top">
+                <!-- Sidebar toggle button-->
+                <a href="#!" data-toggle="offcanvas" class="sidebar-toggle"></a>
+                <!-- Navbar Right Menu-->
+                <div class="navbar-custom-menu f-right">
+                <ul class="top-nav">          
+                    <!-- window screen -->
+                    <!-- <li class="pc-rheader-submenu">
+                        <a href="#!" class="drop icon-circle" onclick="javascript:toggleFullScreen()">
+                            <i class="icon-size-fullscreen"></i>
+                        </a>
 
-                  </li> -->
-                  <!-- User Menu-->
-                  <li class="dropdown">
-                     <a href="#!" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle drop icon-circle drop-image">
-                        <span><img class="img-circle " src="../images/avatar.png" style="width:40px;" alt="User Image"></span>
-                        <span><b><?php echo ucwords($user['fullname']); ?></b> <i class=" icofont icofont-simple-down"></i></span>
+                    </li> -->
+                    <!-- User Menu-->
+                    <li class="dropdown">
+                        <a href="#!" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle drop icon-circle drop-image">
+                            <span><img class="img-circle " src="../images/avatar.png" style="width:40px;" alt="User Image"></span>
+                            <span><b><?php echo ucwords($user['fullname']); ?></b> <i class=" icofont icofont-simple-down"></i></span>
 
-                     </a>
-                     <ul class="dropdown-menu settings-menu">
-                        <!-- <li><a href="#!"><i class="icon-settings"></i> Settings</a></li> -->
-                        <a href="profile" style="color:gray;"><li><i class="icon-user"></i> Profile</li></a>
-                        <a href="<?=SITE_URL; ?>/logout" onclick="return confirm('Do you really want to Logout?. '); " style="color:gray;"><li><i class="icon-logout"></i> Logout</li></a>
-                     </ul>
-                  </li>
-               </ul>
-            </div>
-         </nav>
-      </header>
-      <!-- Side-Nav-->
-      <aside class="main-sidebar hidden-print ">
-         <section class="sidebar" id="sidebar-scroll">
-            <!-- Sidebar Menu-->
-            <ul class="sidebar-menu">
-                <li class="active treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>">
-                        <i class="icon-speedometer"></i><span> Dashboard</span>
-                    </a>                
-                </li>
-               <?php if ($role == 'super_admin') { ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?admins">
-                        <i class="icon-user"></i><span> Admins</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?users">
-                        <i class="icon-user"></i><span> Users</span>
-                    </a>                
-                </li>
-                <?php } elseif ($role == 'admin') { ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?officers">
-                        <i class="icon-user"></i><span> Officers</span>
-                    </a>                
-                </li>
-                <?php } ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
-                        <i class="icon-map"></i><span> Roads</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?traffic_points">
-                        <i class="icon-map"></i><span> Traffic Points</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" onclick="return confirm('Do you really want to Logout?. '); " href="<?=SITE_URL; ?>/logout">
-                        <i class="icon-power"></i><span> Logout</span>
-                    </a>                
-                </li>
-            </ul>
-         </section>
-      </aside>
+                        </a>
+                        <ul class="dropdown-menu settings-menu">
+                            <!-- <li><a href="#!"><i class="icon-settings"></i> Settings</a></li> -->
+                            <a href="profile" style="color:gray;"><li><i class="icon-user"></i> Profile</li></a>
+                            <a href="<?=SITE_URL; ?>/logout" onclick="return confirm('Do you really want to Logout?. '); " style="color:gray;"><li><i class="icon-logout"></i> Logout</li></a>
+                        </ul>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- Side-Nav-->
+        <aside class="main-sidebar hidden-print">
+            <section class="sidebar" id="sidebar-scroll">
+                <!-- Sidebar Menu-->
+                <ul class="sidebar-menu">
+                    <li class="active treeview">
+                        <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>">
+                            <i class="icon-speedometer"></i><span> Dashboard</span>
+                        </a>                
+                    </li>
+                    <?php if ($role == 'super_admin') { ?>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?admins">
+                                <i class="icon-user"></i><span> Admins</span>
+                            </a>                
+                        </li>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?users">
+                                <i class="icon-user"></i><span> Users</span>
+                            </a>                
+                        </li>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?officers">
+                                <i class="icon-user"></i><span> Officers</span>
+                            </a>                
+                        </li>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
+                                <i class="icon-map"></i><span> Roads</span>
+                            </a>                
+                        </li>
+                    <?php } elseif ($role == 'admin') { ?>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?officers">
+                                <i class="icon-user"></i><span> Officers</span>
+                            </a>                
+                        </li>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
+                                <i class="icon-map"></i><span> Roads</span>
+                            </a>                
+                        </li>
+                    <?php } elseif ($role == 'officer') { ?>
+                        <li class="treeview">
+                            <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
+                                <i class="icon-map"></i><span> Roads</span>
+                            </a>                
+                        </li>
+                    <?php } ?>
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?traffic_points">
+                            <i class="icon-map"></i><span> Traffic Points</span>
+                        </a>                
+                    </li>
+                    <li class="treeview">
+                        <a class="waves-effect waves-dark" onclick="return confirm('Do you really want to Logout?. '); " href="<?=SITE_URL; ?>/logout">
+                            <i class="icon-power"></i><span> Logout</span>
+                        </a>                
+                    </li>
+                </ul>
+            </section>
+        </aside>
+    </div>
+</body>
+      
+      
 <?php 
 include 'super_admin/addroad.php'; 
 include 'super_admin/addadmin.php';
@@ -153,65 +173,4 @@ include 'officer/add-traffic-point-three.php';
 include 'add-traffic-point-four.php';
 ?>
 
-<aside class="main-sidebar hidden-print">
-    <section class="sidebar" id="sidebar-scroll">
-        <!-- Sidebar Menu-->
-        <ul class="sidebar-menu">
-            <li class="active treeview">
-                <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>">
-                    <i class="icon-speedometer"></i><span> Dashboard</span>
-                </a>                
-            </li>
-            <?php if ($role == 'super_admin') { ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?admins">
-                        <i class="icon-user"></i><span> Admins</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?users">
-                        <i class="icon-user"></i><span> Users</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?officers">
-                        <i class="icon-user"></i><span> Officers</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
-                        <i class="icon-map"></i><span> Roads</span>
-                    </a>                
-                </li>
-            <?php } elseif ($role == 'admin') { ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?officers">
-                        <i class="icon-user"></i><span> Officers</span>
-                    </a>                
-                </li>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
-                        <i class="icon-map"></i><span> Roads</span>
-                    </a>                
-                </li>
-            <?php } elseif ($role == 'officer') { ?>
-                <li class="treeview">
-                    <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?roads">
-                        <i class="icon-map"></i><span> Roads</span>
-                    </a>                
-                </li>
-            <?php } ?>
-            <li class="treeview">
-                <a class="waves-effect waves-dark" href="<?=HOME_URL; ?>?traffic_points">
-                    <i class="icon-map"></i><span> Traffic Points</span>
-                </a>                
-            </li>
-            <li class="treeview">
-                <a class="waves-effect waves-dark" onclick="return confirm('Do you really want to Logout?. '); " href="<?=SITE_URL; ?>/logout">
-                    <i class="icon-power"></i><span> Logout</span>
-                </a>                
-            </li>
-        </ul>
-    </section>
-</aside>
 
